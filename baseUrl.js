5145b13124484b272b447cd92097c30270b9d6fb
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-04-04 16:15:24 +0530

    add vercel link

--- baseUrl.js
+++ baseUrl.js
@@ -4,7 +4,7 @@ let baseUrl = "";
 if (process.env.NODE_ENV == "development") {
   baseUrl = "http://localhost:3000/";
 } else {
-  baseUrl = "https://www.gauravnarnaware.com";
+  baseUrl = "https://itsgaurav.vercel.app";
 }
 
 export default baseUrl;
