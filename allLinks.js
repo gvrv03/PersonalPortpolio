efd36d2370e897386a7d7f3b4aee37e583657652
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-04-05 12:49:23 +0530

    Finally find solution for api rooutes

--- allLinks.js
+++ allLinks.js
@@ -2,3 +2,4 @@ import baseUrl from "./baseUrl";
 
 export const getAllBlogsURL = baseUrl + "api/getblogs";
 export const getSingleURL = baseUrl + "api/post/";
+export const incView = baseUrl + "api/viewsIn";
