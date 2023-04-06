0b89857f1fa7cb52d8dab7b5e2a37fc4b7c4f213
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-04-06 22:54:49 +0530

    Create Firebase error return fun 3

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,22 +1,23 @@
-import Router from "next/router";
 import React from "react";
 import { useUserAuth } from "../../lib/Context/UserAuthContext";
 
 const DashboardLayout = ({ children }) => {
   const { user } = useUserAuth();
-  if (!user || user.emailVerified == true) {
-    Router.push("/");
+
+  if (user && user.emailVerified) {
+    return (
+      <div className=" pt-24 h-full flex gap-5 container m-auto ">
+        <div className="w-1/5  md:block hidden p-5 bg-white dark:bg-gray-900">
+          hey mam
+        </div>
+        <div className="md:w-4/5 w-full p-5 bg-white dark:bg-gray-900">
+          {children}
+        </div>
+      </div>
+    );
+  } else {
+    return <div className=" pt-24 h-full  container m-auto ">Login First</div>;
   }
-  return (
-    <div className=" pt-24 h-full flex gap-5 container m-auto ">
-      <div className="w-1/5  md:block hidden p-5 bg-white dark:bg-gray-900">
-        hey mam
-      </div>
-      <div className="md:w-4/5 w-full p-5 bg-white dark:bg-gray-900">
-        {children}
-      </div>
-    </div>
-  );
 };
 
 export default DashboardLayout;
